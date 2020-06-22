4cdf96e96559874ad2251449e8abf89cf542ae59
Thiago Matheus <thiago.matheus@cyberlabs.com.br>
2020-06-21 23:36:49 -0300

    Clipped DDQN quase implementada. Fazendo ajustes no caĺculo dos gradientes

--- atari_clipped_ddqn.py
+++ atari_clipped_ddqn.py
@@ -9,7 +9,7 @@ from common.wrappers import make_atari, wrap_deepmind, wrap_pytorch
 from config import Config
 from core.util import get_class_attr_val
 from model import CnnDQN
-from trainer import Trainer
+from trainer_clipped import Trainer
 
 class CnnDDQNAgent:
     def __init__(self, config: Config):
@@ -17,21 +17,33 @@ class CnnDDQNAgent:
         self.is_training = True
         self.buffer = ReplayBuffer(self.config.max_buff)
 
-        self.model = CnnDQN(self.config.state_shape, self.config.action_dim)
-        self.target_model = CnnDQN(self.config.state_shape, self.config.action_dim)
-        self.target_model.load_state_dict(self.model.state_dict())
-        self.model_optim = Adam(self.model.parameters(), lr=self.config.learning_rate)
+        # Instance model 1, target network 1 and the optimizer
+        self.model1 = CnnDQN(self.config.state_shape, self.config.action_dim)
+        self.target_model1 = CnnDQN(self.config.state_shape, self.config.action_dim)
+
+        self.target_model1.load_state_dict(self.model1.state_dict())
+
+        self.model_optim1 = Adam(self.model1.parameters(), lr=self.config.learning_rate)
+
+        # Instance model 2, target network 2 and the optimizer
+        self.model2 = CnnDQN(self.config.state_shape, self.config.action_dim)
+        self.target_model2 = CnnDQN(self.config.state_shape, self.config.action_dim)
+
+        self.target_model2.load_state_dict(self.model2.state_dict())
+
+        self.model_optim2 = Adam(self.model2.parameters(), lr=self.config.learning_rate)
 
         if self.config.use_cuda:
             self.cuda()
 
     def act(self, state, epsilon=None):
+        # Network 1 makes the action
         if epsilon is None: epsilon = self.config.epsilon_min
         if random.random() > epsilon or not self.is_training:
             state = torch.tensor(state, dtype=torch.float).unsqueeze(0)
             if self.config.use_cuda:
                 state = state.cuda()
-            q_value = self.model.forward(state)
+            q_value = self.model1.forward(state)
             action = q_value.max(1)[1].item()
         else:
             action = random.randrange(self.config.action_dim)
@@ -53,31 +65,60 @@ class CnnDDQNAgent:
             r = r.cuda()
             done = done.cuda()
 
-        q_values = self.model(s0).cuda()
-        next_q_values = self.model(s1).cuda()
-        next_q_state_values = self.target_model(s1).cuda()
+        # Calculates Q-value for Network 1
+        q_values1 = self.model1(s0).cuda()
+        next_q_values1 = self.model1(s1).cuda()
+        next_q_state_values1 = self.target_model1(s1).cuda()
 
-        q_value = q_values.gather(1, a.unsqueeze(1)).squeeze(1)
-        next_q_value = next_q_state_values.gather(1, next_q_values.max(1)[1].unsqueeze(1)).squeeze(1)
-        # print('='*60)
-        # print('Targets:')
-        # print(next_q_value)
-        expected_q_value = r + self.config.gamma * next_q_value * (1 - done)
+        q_value1 = q_values1.gather(1, a.unsqueeze(1)).squeeze(1)
+        next_q_value1 = next_q_state_values1.gather(1, next_q_values1.max(1)[1].unsqueeze(1)).squeeze(1)
+
+        # Calculates Q-value for Network 2
+        q_values2 = self.model2(s0).cuda()
+        next_q_values2 = self.model2(s1).cuda()
+        next_q_state_values2 = self.target_model2(s1).cuda()
+
+        q_value2 = q_values2.gather(1, a.unsqueeze(1)).squeeze(1)
+        next_q_value2 = next_q_state_values2.gather(1, next_q_values2.max(1)[1].unsqueeze(1)).squeeze(1)
+
+        print('='*60)
+        print('Targets:')
+        print(next_q_value1, next_q_value2)
+        next_q_values = torch.tensor([next_q_value1, next_q_value2], dtype=torch.float)
+        index = torch.argmin(next_q_values)
+        #next_q_value = torch.min(next_q_value1, next_q_value2)
+
+        # Calculate the target
+        expected_q_value = r + self.config.gamma * next_q_values[index] * (1 - done)
         # Notice that detach the expected_q_value
-        loss = (q_value - expected_q_value.detach()).pow(2).mean()
+        expected_q_value = expected_q_value.detach()
+        # Calculate the loss for network1
+        loss1 = (q_value1 - expected_q_value).pow(2).mean()
+        # Calculate the loss for network2
+        loss2 = (q_value2 - expected_q_value).pow(2).mean()
 
-        self.model_optim.zero_grad()
-        loss.backward()
-        self.model_optim.step()
+        self.model_optim1.zero_grad()
+        loss1.backward()
+        self.model_optim1.step()
+
+        self.model_optim2.zero_grad()
+        loss2.backward()
+        self.model_optim2.step()
 
         if fr % self.config.update_tar_interval == 0:
-            self.target_model.load_state_dict(self.model.state_dict())
+            # Update target network 1
+            self.target_model1.load_state_dict(self.model1.state_dict())
+            # Update target network 2
+            self.target_model2.load_state_dict(self.model2.state_dict())
 
-        return loss.item()
+        return loss1.item(), loss2.item(), index
 
     def cuda(self):
-        self.model.cuda()
-        self.target_model.cuda()
+        self.model1.cuda()
+        self.target_model1.cuda()
+
+        self.model2.cuda()
+        self.target_model2.cuda()
 
     def load_weights(self, model_path):
         model = torch.load(model_path)
@@ -87,7 +128,9 @@ class CnnDDQNAgent:
             self.model.load_state_dict(model)
 
     def save_model(self, output, name=''):
-        torch.save(self.model.state_dict(), '%s/model_%s.pkl' % (output, name))
+        torch.save(self.model1.state_dict(), '%s/model_%s.pkl' % (output, name))
+
+        # torch.save(self.model2.state_dict(), '%s/model2/model_%s.pkl' % (output, name))
 
     def save_config(self, output):
         with open(output + '/config.txt', 'w') as f:
@@ -100,14 +143,14 @@ class CnnDDQNAgent:
         os.makedirs(checkpath, exist_ok=True)
         torch.save({
             'frames': fr,
-            'model': self.model.state_dict()
+            'model': self.model1.state_dict()
         }, '%s/checkpoint_fr_%d.tar'% (checkpath, fr))
 
     def load_checkpoint(self, model_path):
         checkpoint = torch.load(model_path)
         fr = checkpoint['frames']
-        self.model.load_state_dict(checkpoint['model'])
-        self.target_model.load_state_dict(checkpoint['model'])
+        self.model1.load_state_dict(checkpoint['model'])
+        self.target_model1.load_state_dict(checkpoint['model'])
         return fr
 
 
@@ -115,6 +158,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--train', dest='train', action='store_true', help='train model')
+    parser.add_argument('--clipped', type=int, default=0, help='Set 1 to train clipped ddqn model. Otherwise 0')
     parser.add_argument('--env', default='PongNoFrameskip-v4', type=str, help='gym environment')
     parser.add_argument('--test', dest='test', action='store_true', help='test model')
     parser.add_argument('--retrain', dest='retrain', action='store_true', help='retrain model')
